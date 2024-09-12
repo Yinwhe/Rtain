a25b3ce712285b9ea8239b1f214a3b5d066e9e95
Yinwhe <wyffeiwhe@gmail.com>
2024-09-12 14:38:00 +0800

    updates

---
+++ src/container/init.rs
@@ -0,0 +1,116 @@
+use std::{ffi::CString, os::fd::AsRawFd, process::exit};
+
+use log::{error, info};
+use nix::{
+    mount::{mount, MsFlags},
+    sched::{clone, CloneFlags},
+    sys::wait::waitpid,
+    unistd::{execv, Pid},
+};
+
+// When run a container command, it first creates a new process with new
+// namespaces and then runs the init command.
+pub fn run(command: String) {
+    let child = match new_parent_process(true, &command) {
+        Ok(child) => child,
+        Err(err) => {
+            error!("Failed to create new namespace process: {:?}", err);
+            exit(-1);
+        }
+    };
+
+    match waitpid(child, None) {
+        Ok(status) => {
+            info!("Child process exited with status: {:?}", status);
+        }
+        Err(err) => {
+            error!("Failed to wait for child process: {:?}", err);
+            exit(-1);
+        }
+    }
+}
+
+// This is the first process in the new namespace.
+pub fn init(command: String) -> Result<(), Box<dyn std::error::Error>> {
+    info!("Init Command: {}", command);
+
+    let default_mount_flags = MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV;
+
+    // Mount /proc
+    mount(
+        Some("proc"),
+        "/proc",
+        Some("proc"),
+        default_mount_flags,
+        None::<&str>,
+    )?;
+
+    let command_cstr = CString::new(command)?;
+    let args_cstr: Vec<CString> = Vec::new();
+
+    execv(&command_cstr, &args_cstr)?;
+
+    Ok(())
+}
+
+fn new_parent_process(tty: bool, command: &str) -> Result<Pid, nix::Error> {
+    let flags = CloneFlags::CLONE_NEWUTS
+        | CloneFlags::CLONE_NEWPID
+        | CloneFlags::CLONE_NEWNS
+        | CloneFlags::CLONE_NEWNET
+        | CloneFlags::CLONE_NEWIPC;
+
+    const STACK_SIZE: usize = 1024 * 1024;
+    let mut child_stack: Vec<u8> = vec![0; STACK_SIZE];
+
+    // clone new child process
+    let child_pid = unsafe {
+        clone(
+            Box::new(|| child_func(tty, command)),
+            &mut child_stack,
+            flags,
+            None,
+        )
+    }?;
+
+    Ok(child_pid)
+}
+
+fn child_func(tty: bool, command: &str) -> isize {
+    // if enable tty, inherit the tty from parent
+    if tty {
+        let stdin_fd = std::io::stdin().as_raw_fd();
+        let stdout_fd = std::io::stdout().as_raw_fd();
+        let stderr_fd = std::io::stderr().as_raw_fd();
+
+        unsafe {
+            use nix::libc::dup2;
+            if dup2(stdin_fd, 0) == -1 {
+                error!("Failed to dup2 stdin");
+                return -1;
+            }
+            if dup2(stdout_fd, 1) == -1 {
+                error!("Failed to dup2 stdout");
+                return -1;
+            }
+            if dup2(stderr_fd, 2) == -1 {
+                error!("Failed to dup2 stderr");
+                return -1;
+            }
+        }
+    }
+
+    let args = vec![
+        CString::new("init").unwrap(),
+        CString::new(command).unwrap(),
+    ];
+
+    let prog = CString::new("/proc/self/exe").unwrap();
+    match execv(&prog, &args) {
+        Ok(_) => 0,
+        Err(err) => {
+            error!("Failed to execv: {:?}", err);
+            -1
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::fs;
-use std::process::{exit, Command};
-use std::os::unix::prelude::CommandExt;
+use std::env;
+use std::process::exit;
 
 use clap::{Parser, Subcommand};
-use nix::sched::{self, CloneFlags};
+use log::{error, info};
+
+mod container;
+use container::{init, run};
 
 #[derive(Parser, Debug)]
 #[command(name = "rtain")]
@@ -15,82 +17,27 @@ struct CLI {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Initializes something
-    Init,
+    Init { command: String },
 
-    /// Runs a command with optional flags
-    Run {
-        /// Command to run
-        command: Option<String>,
-    },
+    Run { command: String },
 }
 
 fn main() {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
     let cli = CLI::parse();
 
     match cli.command {
         Commands::Run { command } => {
-            if let Some(c) = command {
-                run(c);
-            } else {
-                println!("No command provided.");
+            run(command);
+        }
+        Commands::Init { command } => {
+            info!("Initializing container...");
+            if let Err(e) = init(command) {
+                error!("Failed to initialize container: {:?}", e);
+                exit(-1);
             }
         }
-        Commands::Init => {
-            println!("Initializing container...");
-        }
     }
-}
-
-fn run(command: String) {
-    let mut parent = new_parent_process(command);
-    match parent.spawn() {
-        Ok(mut child) => {
-            match child.wait() {
-                Ok(status) => {
-                    if !status.success() {
-                        println!("Child process exited with error: {:?}", status);
-                        exit(-1);
-                    }
-                }
-                Err(e) => {
-                    println!("Failed to wait for child process: {:?}", e);
-                    exit(-1);
-                }
-            }
-
-        }
-        Err(e) => {
-            println!("Failed to start parent process: {:?}", e);
-            exit(-1);
-        }
-    }
-}
-
-fn new_parent_process(command: String) -> Command {
-    let args = vec!["init", command.as_str()];
-
-    // create new namespaces
-    let mut cmd = Command::new("/proc/self/exe");
-    cmd.args(&args);
-
-    unsafe {
-        cmd.pre_exec(|| {
-            // 设置命名空间
-            sched::unshare(
-                CloneFlags::CLONE_NEWUTS
-                    | CloneFlags::CLONE_NEWPID
-                    | CloneFlags::CLONE_NEWNS
-                    | CloneFlags::CLONE_NEWNET
-                    | CloneFlags::CLONE_NEWIPC,
-            )?;
-            Ok(())
-        });
-    }
-
-    cmd.stdin(fs::File::open("/dev/tty").unwrap());
-    cmd.stdout(fs::File::open("/dev/tty").unwrap());
-    cmd.stderr(fs::File::open("/dev/tty").unwrap());
-
-    cmd
 }
